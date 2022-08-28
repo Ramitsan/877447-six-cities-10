a436965936219d1b7ab36992d4fe9fb07c0209a4
Ramitsan <ramitsan@gmail.com>
2022-08-29 00:58:13 +0300

    Добавляет отрисовку пустого списка избранного

--- project/src/pages/favorites-page/favorites-page.tsx
+++ project/src/pages/favorites-page/favorites-page.tsx
@@ -1,33 +1,36 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import FavoriteCardList from '../../components/favorite-card-list/favorite-card-list';
 import { OfferType } from '../../types/offerType';
 import Header from '../../components/header/header';
 import { useAppSelector } from '../../hooks';
+import PageFavoriteEmpty from '../favorite-page-empty/favorite-page-empty';
 
 type FavoritesPageProps = {
   offers: OfferType[];
 };
 
-export default function FavoritesPage({offers} : FavoritesPageProps): JSX.Element {
+export default function FavoritesPage({ offers }: FavoritesPageProps): JSX.Element {
   const favoriteOffers = useAppSelector((state) => state.favoriteOffers);
 
   return (
-    <div className="page">
-      <Header />
+    favoriteOffers.length === 0 ? <PageFavoriteEmpty /> :
 
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <FavoriteCardList favoritesOffers={favoriteOffers} />
-          </section>
-        </div>
-      </main>
-      <footer className="footer container">
-        <Link className="footer__logo-link" to="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </Link>
-      </footer>
-    </div>
+      <div className="page">
+        <Header />
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <FavoriteCardList favoritesOffers={favoriteOffers} />
+            </section>
+          </div>
+        </main>
+
+        <footer className="footer container">
+          <Link className="footer__logo-link" to="main.html">
+            <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+          </Link>
+        </footer>
+      </div>
   );
 }
