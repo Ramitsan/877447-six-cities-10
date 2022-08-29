8d998d2ef872dd8cf9508748ab8aadfa7810ae60
Ramitsan <ramitsan@gmail.com>
2022-08-29 20:47:36 +0300

    Добавляет коммнтарий

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,9 +1,9 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { AuthorizationStatus, DEFAULT_CITY } from '../const';
-// import { offers } from '../mocks/offers';
-import { changeCity, loadOffers, setDataLoadedStatus, requireAuthorization, setError, setUserData } from './actions';
+import { changeCity, loadOffers, setDataLoadedStatus, requireAuthorization, setError, setUserData, loadFavoriteOffers, updateOffer } from './actions';
 import { OfferType } from '../types/offerType';
 import { UserDataType } from '../types/user-data';
+import { CommentType } from '../types/commentType';
 
 type InitalStateType = {
   city: string;
@@ -12,13 +12,18 @@ type InitalStateType = {
   isDataLoaded: boolean,
   error: string | null,
   userData: UserDataType | null,
+  comments: CommentType[],
+  favoriteOffers: OfferType[],
 }
 
 // Объект начального состояния:
 // город (используется для отбора списка предложений в определённом городе),
 // список предложений по аренде,
 // статус авторизации
-// и текст ошибки
+// текст ошибки
+// данные пользователя
+// комментарии
+// избранные офферы
 const initialState: InitalStateType = {
   city: DEFAULT_CITY,
   offers: [],
@@ -26,6 +31,8 @@ const initialState: InitalStateType = {
   isDataLoaded: false,
   error: null,
   userData: null,
+  comments: [],
+  favoriteOffers: [],
 };
 
 //Функция-редьюсер. Она принимает в качестве параметров текущий state и действие (action).
@@ -49,5 +56,15 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(setUserData, (state, action) => {
       state.userData = action.payload;
+    })
+    .addCase(loadFavoriteOffers, (state, action) => {
+      state.favoriteOffers = action.payload;
+    })
+    // получаем оффер с обновленным статусом избранного и подменяем в списке офферов
+    .addCase(updateOffer, (state, action) => {
+      const offers = [...state.offers];
+      const index = offers.findIndex((offer) => offer.id === action.payload.id);
+      offers[index] = action.payload;
+      state.offers = offers;
     });
 });
