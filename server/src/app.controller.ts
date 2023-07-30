bb6e5b2d17d377fbfc881c8f9c75581bb646b360
Ramitsan <ramitsan@gmail.com>
2023-07-30 23:43:12 +0300

    get hotels images and users avatars

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -3,6 +3,9 @@ import { AppService } from './app.service';
 import { OfferType } from '../../project/src/types/offerType';
 import { hotels } from './data/hotels';
 import { comments } from './data/comments';
+import fetch from 'node-fetch';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 interface IUser  {
   "id": number,
@@ -23,8 +26,44 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    // await fs.mkdir(path.join(__dirname, 'data/static/hotel'), {
+    //   recursive: true,
+    // });
+
+    // const images = new Array(19).fill(0).map((_, index) => {
+    //   fetch(`https://10.react.pages.academy/static/hotel/${index + 1}.jpg`).then(res => res.blob()).then((blob: Blob) => blob.arrayBuffer())
+    //   .then((img: ArrayBuffer) => {
+    //     console.log(img);
+
+    //     fs.writeFile(path.join(__dirname, 'data/static/hotel', `${index + 1}.jpg`), Buffer.from(img));
+    //   });
+    // })
+    //  return this.appService.getHello();
+
+  //   const users = [];
+  //   comments.forEach(comment => {
+  //     comment.comments.forEach(it => {
+  //       if(users.find(user => user.id == it.user.id) == null) {
+  //         users.push(it.user);
+  //       }
+  //     })
+  //   })
+  //  return users;
+
+  await fs.mkdir(path.join(__dirname, 'data/static/avatar'), {
+    recursive: true,
+  });
+
+  const images = new Array(9).fill(0).map((_, index) => {
+    fetch(`https://10.react.pages.academy/static/avatar/${index + 1}.jpg`).then(res => res.blob()).then((blob: Blob) => blob.arrayBuffer())
+    .then((img: ArrayBuffer) => {
+      console.log(img);
+
+      fs.writeFile(path.join(__dirname, 'data/static/avatar', `${index + 1}.jpg`), Buffer.from(img));
+    });
+  })
+   return this.appService.getHello();
   }
 
   @Get('hotels')
